977c796662ed224b6e2faa5c429ee349b70337dd
davidpinkevich <davidpinkevich95@gmail.com>
2023-10-22 13:58:13 +0300

    feat: add logic for buttons

--- src/redux/slice/index.ts
+++ src/redux/slice/index.ts
@@ -3,11 +3,19 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 type TInitialState = {
   tooltip: boolean;
   play: boolean;
+  panelHidden: boolean;
+  personalData: boolean;
+  focusButton: string | null;
+  phoneNumber: Array<string>;
 };
 
 const initialState: TInitialState = {
   tooltip: false,
   play: false,
+  panelHidden: true,
+  personalData: false,
+  focusButton: null,
+  phoneNumber: [],
 };
 
 const infoSlice = createSlice({
@@ -20,8 +28,33 @@ const infoSlice = createSlice({
     changePlay(state, action: PayloadAction<boolean>) {
       state.play = action.payload;
     },
+    changeHidden(state, action: PayloadAction<boolean>) {
+      state.panelHidden = action.payload;
+    },
+    changePersonalData(state, action: PayloadAction<boolean>) {
+      state.personalData = action.payload;
+    },
+    changeFocus(state, action: PayloadAction<string | undefined | null>) {
+      if (typeof action.payload === 'string') {
+        state.focusButton = action.payload;
+      }
+    },
+    changeNumber(state, action: PayloadAction<string>) {
+      if (action.payload === 'clear') {
+        state.phoneNumber = [];
+      } else {
+        state.phoneNumber.push(action.payload);
+      }
+    },
   },
 });
 
 export default infoSlice.reducer;
-export const { viewTooltip, changePlay } = infoSlice.actions;
+export const {
+  viewTooltip,
+  changePlay,
+  changeFocus,
+  changeNumber,
+  changeHidden,
+  changePersonalData,
+} = infoSlice.actions;
