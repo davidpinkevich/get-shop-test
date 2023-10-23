b16492fb5ee2f639baa2234e55d74f1887ca5d0d
davidpinkevich <davidpinkevich95@gmail.com>
2023-10-23 20:27:11 +0300

    fix: create composition for side

--- src/components/Side/Side.tsx
+++ src/components/Side/Side.tsx
@@ -1,15 +1,18 @@
-import { useRef } from 'react';
-import ButtonNumber from '../buttons/ButtonNumber/ButtonNumber';
-import { PANEL } from '../../constants';
+import { useAppSelector } from '../../hooks';
+import InputNumber from '../InputNumber/InputNumber';
+import Panel from '../Panel/Panel';
+import PersonalData from '../PersonalData/PersonalData';
 import './Side.scss';
 
 export const Side = () => {
-  const ref = useRef<HTMLButtonElement | null>(null);
+  const hidden = useAppSelector((state) => state.infoReducer.panelHidden);
   return (
-    <div className="side">
-      {PANEL.map((item, index) => {
-        return <ButtonNumber ref={ref} key={index} id={item} />;
-      })}
+    <div className={hidden ? 'side' : 'side view'}>
+      <h2 className="side__title">Введите ваш номер мобильного телефона</h2>
+      <InputNumber />
+      <p className="side__info">и с Вами свяжется наш менеждер для дальнейшей консультации</p>
+      <Panel />
+      <PersonalData />
     </div>
   );
 };
