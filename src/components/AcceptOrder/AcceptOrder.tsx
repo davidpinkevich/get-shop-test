dfed1141124a989e208e00b8e3ff2db5d9cc1429
davidpinkevich <davidpinkevich95@gmail.com>
2023-10-24 11:11:06 +0300

    feat: add accept order

---
+++ src/components/AcceptOrder/AcceptOrder.tsx
@@ -0,0 +1,15 @@
+import './AcceptOrder.scss';
+
+const AcceptOrder = () => {
+  return (
+    <div className="side__accept">
+      <h2 className="side__accept-title">Заявка принята</h2>
+      <p className="side__accept-info">
+        Держите телефон под рукой.
+        <br /> Скоро с Вами свяжется наш менеджер.
+      </p>
+    </div>
+  );
+};
+
+export default AcceptOrder;
